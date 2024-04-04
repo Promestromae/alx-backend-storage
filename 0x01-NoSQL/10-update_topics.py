66fc20ef8a02782d6406bb143a2b8a81987fd347
Promestromae <nzuvameshack@gmail.com>
2024-04-04 11:04:55 +0000

    vestiaire

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,13 +1,10 @@
-#!ange school topic
-"""
+#!/usr/bin/env python3
+""" Update topics of a school document based on the name """
 import pymongo
 
 
 def update_topics(mongo_collection, name, topics):
-    """
-    update many rows
-    """
-    return mongo_collection.update_many(
-        {"name": name},
-        {"$set": {"topics": topics}}
-    )
+    """ Update topics of a school document based on the name """
+    if not mongo_collection:
+        return None
+    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
